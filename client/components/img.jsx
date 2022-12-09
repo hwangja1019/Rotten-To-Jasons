546fc7fc7c2412590dca98be47acaae1ac88bff5
hwangja1019@gmail.com <hwangja1019@gmail.com>
2022-12-09 09:22:52 -0500

    very minor change

--- client/components/img.jsx
+++ client/components/img.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
-const Image = (props) =>{
-    return (
-        <img className="image" src={require("../../imgs/tomatoMe.png").default} alt="no-face" />
-    )
-}
-
+const Image = (props) => {
+  return (
+    <img
+      className='image'
+      src={require('../../imgs/tomatoMe.png').default}
+      alt='no-face'
+    />
+  );
+};
 
 export default Image;
