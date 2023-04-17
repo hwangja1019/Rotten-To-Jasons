2e6df0924d78652fe958f7a4893be19797b66bb7
hwangja1019@gmail.com <hwangja1019@gmail.com>
2023-04-17 10:13:47 -0400

    consistent naming of URI

--- server/model/dramaModels.js
+++ server/model/dramaModels.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const MONGO_URI = process.env.PG_URI;
+const MONGO_URI = process.env.MONGO_URI;
 
 mongoose
   .connect(MONGO_URI, {
